8e8fb6d61fcfa133d26fb38312b9e9af46934afb
ashuxen <79180069+ashuxen@users.noreply.github.com>
2022-12-10 14:29:44 -0800

    helmet_app_final
    helmet_app_final

--- helmet_app_final.py
+++ helmet_app_final.py
@@ -35,81 +35,34 @@ class Streamlit_YOLOV7(SingleInference_YOLOV7):
         1. predicted_bboxes_PascalVOC   #list#  #list of values for detections containing the following (name,x0,y0,x1,y1,score)
 
         CREDIT
-        Please see https://github.com/WongKinYiu/yolov7.git for Yolov7 resources (i.e. utils/models)
+        1.Please see https://github.com/WongKinYiu/yolov7.git for Yolov7 resources (i.e. utils/models)
         @article{wang2022yolov7,
             title={{YOLOv7}: Trainable bag-of-freebies sets new state-of-the-art for real-time object detectors},
             author={Wang, Chien-Yao and Bochkovskiy, Alexey and Liao, Hong-Yuan Mark},
             journal={arXiv preprint arXiv:2207.02696},
             year={2022}
             }
-         Steven Smiley :  https://github.com/stevensmiley1989/Full_Loop_YOLO by Steven Smiley 2022/11/24
+        2. Steven Smiley :  https://github.com/stevensmiley1989/Full_Loop_YOLO by Steven Smiley 2022/11/24
         '''
+
         super().__init__(img_size, path_yolov7_weights, path_img_i, device_i=device_i)
 
     def main(self):
         st.title('Custom YoloV7 Object Detector')
+        st.write("By: Ashutosh,Raj & Sundar")
         st.subheader(""" Upload an image and run YoloV7 on it.  
         This model was trained to detect hard hat helmet. the model fails in some cases.
         (i.e. objects too close up & too far away):\n""")
-        st.markdown(
-            """
-        <style>
-        .reportview-container .markdown-text-container {
-            font-family: monospace;
-        }
-        .sidebar .sidebar-content {
-            background-image: linear-gradient(#2e7bcf,#2e7bcf);
-            color: black;
-        }
-        .Widget>label {
-            color: green;
-            font-family: monospace;
-        }
-        [class^="st-b"]  {
-            color: green;
-            font-family: monospace;
-        }
-        .st-bb {
-            background-color: black;
-        }
-        .st-at {
-            background-color: green;
-        }
-        footer {
-            font-family: monospace;
-        }
-        .reportview-container .main footer, .reportview-container .main footer a {
-            color: black;
-        }
-        header .decoration {
-            background-image: None);
-        }
 
-
-        </style>
-        """,
-            unsafe_allow_html=True,
-        )
-        st.markdown(
-            """
-            <style>
-            .reportview-container {
-                background: url("")
-            }
-        .sidebar .sidebar-content {
-                background: url("")
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
         text_i_list = []
         for i, name_i in enumerate(self.names):
             # text_i_list.append(f'id={i} \t \t name={name_i}\n')
             text_i_list.append(f'{i}: {name_i}\n')
         st.selectbox('Classes', tuple(text_i_list))
+        #helmet_weight= "/Users/ashutoshkumar/Desktop/PGP-AIML/AAI-521/Module_7/yolov7/weights/best.pt"
+        #object_weight= "/Users/ashutoshkumar/Desktop/PGP-AIML/AAI-521/Module_7/yolov7/weights/yolov7-tiny.pt"
         self.conf_selection = st.selectbox('Confidence Threshold', tuple([0.1, 0.25, 0.5, 0.75, 0.95]))
-
+        #self.weight_selection = st.selectbox('Confidence Threshold', tuple([helmet_weight,object_weight]))
         self.response = requests.get(self.path_img_i)
 
         self.img_screen = Image.open(BytesIO(self.response.content))
@@ -119,8 +72,9 @@ class Streamlit_YOLOV7(SingleInference_YOLOV7):
         st.markdown('YoloV7 on streamlit for Object detection.')
         self.im0 = np.array(self.img_screen.convert('RGB'))
         self.load_image_st()
-        predictions = st.button('Predict Objects on the image?')
+        predictions = st.button('Detect Object on the image?')
         if predictions:
+            #path_yolov7_weights = "/Users/ashutoshkumar/Desktop/PGP-AIML/AAI-521/Module_7/yolov7/weights/best.pt"
             self.predict()
             predictions = False
 
@@ -141,6 +95,7 @@ class Streamlit_YOLOV7(SingleInference_YOLOV7):
 
     def predict(self):
         self.conf_thres = self.conf_selection
+
         st.write('Loading image file')
         self.load_cv2mat(self.im0)
         st.write('Object detection in progress...')
@@ -164,12 +119,23 @@ if __name__ == '__main__':
 
     # INPUTS for YOLOV7
     img_size = 1056
-    path_yolov7_weights = "weights/best.pt"
-    path_img_i = "https://raw.githubusercontent.com/ashuxen/Helmet_Object_detection/main/test_images/hard_hat_workers1294.png"
+    helmet_weight = "weights/best.pt"
+    object_weight = "weights/yolov7-tiny.pt"
+    weight = st.radio('select the object detection type you like to perform',('Helmet','Generic'),index=0)
+    if weight == 'Helmet':
+        path_yolov7_weights=helmet_weight
+    else:
+        path_yolov7_weights = object_weight
+
+    #weight_selection = st.radio('Select the object detection objective',(helmet_weight,object_weight),index=0)
+    #path_yolov7_weights = "/Users/ashutoshkumar/Desktop/PGP-AIML/AAI-521/Module_7/yolov7/weights/best.pt"
+    #path_yolov7_weights = weight_selection
+    #path_yolov7_weights = "/Users/ashutoshkumar/Desktop/PGP-AIML/AAI-521/Module_7/yolov7/weights/yolov7-tiny.pt"
+    path_img_i = "https://raw.githubusercontent.com/ashuxen/Helmet_Object_detection/main/usd-logo-horizontal.png"
     # INPUTS for webapp
-    app.capt = "Initial Image"
-    app.new_yolo_model(img_size, path_yolov7_weights, path_img_i)
-    app.conf_thres = 0.65
+    app.capt = "Initial project for object detection"
+    app.new_yolo_model(img_size,path_yolov7_weights, path_img_i)
+    #app.conf_thres = 0.65
     app.load_model()  # Load the yolov7 model
 
     app.main()
